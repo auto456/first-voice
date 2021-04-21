decf0a7cf197071d9d12291cd493fc564eb54914
Ar Pen <ar.pen@local>
2021-04-21 11:27:50 +0200

    fixed table; improved download

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -36,7 +36,7 @@
       >
     </b-list-group>
 
-    <b-table striped hover :items="items" :fields="fields">
+    <b-table ref="songTable" striped hover :items="items" :fields="fields">
       <template #cell(title)="items">{{
         items.item.attributes.title
       }}</template>
@@ -67,6 +67,7 @@ export default {
     return {
       plans: [],
       dropDownText: "Gottesdienst wählen",
+      planId: "",
       items: [],
       fields: [
         { key: "title", label: "Titel" },
@@ -96,7 +97,8 @@ export default {
     },
     onDropDownClick(plan) {
       this.dropDownText = plan.attributes.dates;
-      this.getSongs(plan.id);
+      this.planId = plan.id;
+      this.getSongs();
     },
     addNameToList() {
       this.singers.push(this.singer);
@@ -104,12 +106,17 @@ export default {
     },
     addToFirstVoices(data, singer) {
       this.items[data.index].attributes.firstVoice = singer;
-      console.log(this.items);
+      this.$refs.songTable.refresh();
     },
-    exportPDF() {
-      var songs = []
-      this.items.forEach(item => songs.push([item.attributes.title, item.attributes.firstVoice?item.attributes.firstVoice:""]))
-  
+    async exportPDF() {
+      var songs = [];
+      this.items.forEach((item) =>
+        songs.push([
+          item.attributes.title,
+          item.attributes.firstVoice ? item.attributes.firstVoice : "",
+        ])
+      );
+
       const doc = new jsPDF();
 
       doc.text("Sänger: ", 14, 10);
@@ -118,16 +125,61 @@ export default {
 
       doc.autoTable({
         head: [["Titel", "Erste Stimme"]],
-        margin: { top: this.singers.length*10+15},
+        margin: { top: this.singers.length * 10 + 15 },
         body: songs,
       });
-      //console.log(doc.output());
+      var pdf = doc.output("blob");
+      var blob = new Blob([pdf], { type: "application/pdf" });
+
+      var formData = new FormData();
+
+      formData.append("file", blob, "blob.pdf");
+      console.log(formData);
       doc.save("a4.pdf");
+
+      const uploadRes = await fetch(
+        "https://upload.planningcenteronline.com/v2/files",
+        {
+          headers: {
+            Authorization:
+              "Basic MzRmMzY5OWNkMmFkZjc3YmFmOTNlMDJlZjUyYjU3YTYxYjI4MWIyNDcyZjRkZjQwM2E0NDE5ODI3NDM5ZmYyYjpmODBmYmVmNzA2Nzg2NjI4MDY3NTlhOTcyNTBhY2VjMTMxOTFhZGI5Y2Q5NzIxOGY1YjBmYTY0ZDUwYjBlOWVk",
+          },
+          body: formData,
+          method: "POST",
+        }
+      );
+      const uploadData = await uploadRes.json();
+      const uid = uploadData.data[0].id;
+
+      fetch(
+        "https://services.planningcenteronline.com/files/upload?item_type=PlanBase&item_id=" +
+          this.planId,
+        {
+          body: "file%5Bid%5D=" + uid,
+          headers: {
+            Accept: "*/*",
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "Sec-Ch-Ua":
+              '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
+            "Sec-Ch-Ua-Mobile": "?0",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin",
+            "Set-Cookie":
+              "_ga=GA1.2.542467271.1618602085; _gid=GA1.2.616928862.1618602085; filePins=%7B%7D; legacy_plans=0; planning_center_session=eyJhbGciOiJFUzI1NiJ9.eyJqdGkiOiJkYTY4MWQwNzQzZWNiYzIyNmNkNjY1YjlkNDk3ZGQ0ZCIsImlhdCI6MTYxODY1NjE4OSwiZXhwIjoxNjE5ODY1Nzg5LCJpc3MiOiIvcGNvL3Nlc3Npb24ifQ.sEYEtMOA-PU3BQ68dggeQVSDUAVCxmN6WZdIduxTSY4TP-O9trobcIjh9RsrlBAM-hO9Lb8p5gQApBBnc6GQtQ; _account_center_session=Ty9PNEhNcG1ndmhZR3pPclFsYUVtcTdibzk5a0l1Ykh3WElQOEgrbkw1VEhqQWFLUWtIaHNKUmpvTHNQMjA3eGgzWm1Kc1dXZ1hBZnY0Zm1TNnA2bjUraVF3ZnFwUWE2MUozQ0tFUEFTMERxcEhrWFdIbDIvcEhQRU1UQW9uSU9zdUpPdDlZdURoTFUwMkljUWNmeDh2S2phYm5BSnhISjBoQy90RkNQN0FGZ1NnSmgrOStCZDlKYmJsNXlIZ0lIUEVkanF3SVpwZjJHKzRVZjJFMnZrUVgvbkJ5SEJMWHYxdFhlUklTeHloK0RtYWVyRWtZdXZ4NXpXc1RqMm5rbkg2a0kyMElacjM0QUdjdEVRWkNEN05ZaFlTRHV1Uk02SVc2UmNHSGVMakVveWFFcGREekVRVDUrOFNzSVAxOWdWakVYa3NOMlVGT3plSFdkZTA0RlFOSXdYOFVhSlRZMlNlS2J4Vk1Sd0VVWWdZMjdIUDV1dHhxWlpMbUs5WUEwL01ZQlBQRU9LQ0dteUtXeGc1L0FlYysrWURvVnM3cnVWUGNUdG9KSWlpSlFnSmw1UHBVY2pqcHo0bThZMjQyUS0tMTlVZjhGWUIzQUVJL2FoSkJrSmFSUT09--bf63b393922bc4bab1dbcdb39889813432ff735f",
+            "X-Csrf-Token":
+              "Xow5D9fTtH53Wsr4n7VAEdMYOP1RSDn6tQKZ/Aq2BfvKNcH0V3KXvB/J/TNC8RaXQvwG44X4f/9wLEwUez7Xzg==",
+            "X-Pco-Live-Socket-Id": uid,
+            "X-Requested-With": "XMLHttpRequest",
+          },
+          method: "POST",
+        }
+      );
     },
-    async getSongs(planId) {
+    async getSongs() {
       const response = await fetch(
         "https://api.planningcenteronline.com/services/v2/service_types/227874/plans/" +
-          planId +
+          this.planId +
           "/items",
         {
           headers: {
